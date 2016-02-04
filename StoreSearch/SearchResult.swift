c977d7281e16edc680c6041d6e20d04da3cf662e
Shehab Saqib <ShehabSaqib@shehabs-mbp.home>
2016-02-04 21:07:43 +0000

    Added a search bar and table view. The search puts fake results in the table for now.

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Shehab Saqib on 04/02/2016.
+//  Copyright © 2016 Shehab Saqib. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
