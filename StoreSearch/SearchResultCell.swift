9c1ec0766aef3c7ce032c83a923dc783295a4cad
Shehab Saqib <ShehabSaqib@shehabs-mbp>
2016-02-05 19:43:49 +0000

    Using custom cells for search results

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Shehab Saqib on 05/02/2016.
+//  Copyright © 2016 Shehab Saqib. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
