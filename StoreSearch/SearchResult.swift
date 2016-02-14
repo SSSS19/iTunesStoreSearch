e455bb4c30a60003ba637f4507829a730a94cf46
Shehab Saqib <ShehabSaqib@shehabs-mbp.home>
2016-02-14 00:56:42 +0000

    Talk to Web service

--- StoreSearch/SearchResult.swift
+++ StoreSearch/SearchResult.swift
@@ -11,4 +11,19 @@ import Foundation
 class SearchResult {
     var name = ""
     var artistName = ""
+    var artworkURL60 = ""
+    var artworkURL100 = ""
+    var storeURL = ""
+    var kind = ""
+    var currency = ""
+    var price = 0.0
+    var genre = ""
+}
+
+func < (lhs: SearchResult, rhs: SearchResult) -> Bool {
+    return lhs.name.localizedStandardCompare(rhs.name) == .OrderedAscending
+}
+
+func > (lhs: SearchResult, rhs: SearchResult) -> Bool {
+    return rhs.name.localizedStandardCompare(lhs.name) == .OrderedAscending
 }
